ed0e71a4822ab22950013b9bd5dfac8c6ef6cd46
theAdhocracy <murrayadcock@gmail.com>
2025-10-08 00:26:19 +0100

    :sparkles: Date formatting utility

---
+++ src/utilities/formatDate.ts
@@ -0,0 +1,35 @@
+export const formatDate = (date: Date) => {
+	// HTML attribute
+	const html = new Intl.DateTimeFormat("sv-SE")
+		.format(new Date(date))
+		.toString();
+
+	// Formatted date
+	const formatted = new Intl.DateTimeFormat("en-US", {
+		weekday: "long",
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+	})
+		.format(new Date(date))
+		.toString();
+
+	// Datetime
+	const datetime = new Intl.DateTimeFormat("en-US", {
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+		hour: "2-digit",
+		minute: "2-digit",
+	})
+		.format(new Date(date))
+		.toString()
+		.replace(" AM", " am")
+		.replace(" PM", " pm");
+
+	return {
+		html: html,
+		formatted: formatted,
+		datetime: datetime,
+	};
+};
